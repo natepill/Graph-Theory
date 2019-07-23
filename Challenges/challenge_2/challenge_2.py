fcb5d1989b2378fdd982dace37448733bc8870c4
Nathan Pillai <natepill@gmail.com>
2019-07-23 09:40:04 -0700

    [added] graph data txt

---
+++ Challenges/challenge_2/challenge_2.py
@@ -0,0 +1,95 @@
+from graph import Graph, Vertex
+import sys
+
+
+def read_graph(filename):
+    """ Read and parse file containing Graph information"""
+
+    with open(filename, "r") as file:
+        graph_info = file.read().split("\n")
+
+    # Remove extra new line created by Atom
+    del graph_info[-1]
+
+    return graph_info
+
+def parse_edge(edge):
+
+    new_edge = []
+    edge = edge.split(',')
+    # print("edge:",edge)
+
+    for edge_info in edge:
+
+        curr_edge = ''
+        for char in edge_info:
+            if char == '(' or char == ')':
+                continue
+            curr_edge += char
+
+        new_edge.append(int(curr_edge))
+
+    # print("New_edge:", edge)
+    return new_edge
+
+
+def construct_graph(filename):
+    """ Construct graph based on information in the filename """
+
+    graph_info = read_graph(filename) # parsed graph info file
+    #
+    # print("Graph Info:", graph_info)
+    # print(graph_info[2:])
+
+    type_of_graph = graph_info[0] # Graph or DiGraph
+    list_of_verticies = [int(vert) for vert in graph_info[1] if vert.isalnum()] # Verticies to add to the Graph
+    # print("graph_info[2:]:", graph_info[2:])
+    list_of_edges = [parse_edge(edge) for edge in graph_info[2:]] # Describe the edges that connect verticies including possible weight
+
+    graph = Graph()
+
+    # Add verticies to the Graph
+    for vert in list_of_verticies:
+        graph.add_vertex(int(vert))
+
+    # Connect the Verticies with edges
+    for edge_list in list_of_edges:
+
+        if len(edge_list) == 3:
+            graph.add_edge(edge_list[0], edge_list[1], edge_list[2])
+
+        if len(edge_list) == 2:
+            graph.add_edge(edge_list[0], edge_list[1])
+
+
+    return graph
+
+
+def grab_graph_info(graph, filename):
+    """ Return Tuple object with number of vertices, number of edges, and the Edge List"""
+
+    edge_list = open(filename, 'r').read().split("\n")[2:-1]
+    num_of_edges = len(edge_list)
+
+    return [graph.num_vertices, num_of_edges, edge_list]
+
+
+def find_shortest_path(vert1, vert2):
+    """ Breadth-first search to compute the shortest path between two provided vertices """
+
+
+
+
+
+if __name__ == "__main__":
+
+    graph = construct_graph(sys.argv[1])
+    graph_info = grab_graph_info(graph, sys.argv[1])
+
+    print("Vertices:", graph_info[0])
+    print("Edges:", graph_info[1])
+    print("Edge List:")
+    for edge in graph_info[2]:
+        print(edge)
+
+    # print(graph_info)
---
+++ Challenges/challenge_2/graph.py
@@ -0,0 +1,135 @@
+#!python
+
+
+
+class Vertex(object):
+
+    def __init__(self, vertex):
+        """Initialize a vertex and its neighbors.
+        neighbors: set of vertices adjacent to self,
+        stored in a dictionary with key = vertex,
+        value = weight of edge between self and neighbor.
+        """
+        self.id = vertex
+        self.neighbors = {}
+
+
+    def __str__(self):
+            """Output the list of neighbors of this vertex."""
+            return f'{self.id} adjacent to {[x.id for x in self.neighbors]}'
+
+
+    def add_neighbor(self, vertex, weight=0):
+        """Add a neighbor along a weighted edge."""
+        self.neighbors[vertex] = weight
+
+
+    def get_neighbors(self):
+        """Return the neighbors of this vertex."""
+        return self.neighbors.keys()
+
+    def get_id(self):
+        """Return the id of this vertex."""
+        return self.id
+
+    def get_edge_weight(self, vertex):
+        """Return the weight of this edge."""
+        # vertex to the given vertex.
+        return self.neighbors[vertex]
+
+    def get_edges(self):
+        """Return number of edges of one vertex"""
+        return len(self.neighbors.keys())
+
+
+
+
+""" Graph Class
+A class demonstrating the essential
+facts and functionalities of graphs.
+"""
+
+# NOTE: id is the key and vertecies are the values
+class Graph:
+    def __init__(self):
+        """Initialize a graph object with an empty dictionary."""
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        """Iterate over the vertex objects in the graph, to use sytax: for v in g"""
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, key):
+        """Add a new vertex object to the graph with the given key and return the vertex."""
+        # increment the number of vertices
+        self.num_vertices += 1
+        # create a new vertex
+        new_vertex = Vertex(key)
+        # add the new vertex to the vertex dictionry
+        self.vert_dict[key] = new_vertex
+        # return the new vertex
+        return new_vertex
+
+    def get_vertex(self, key):
+        """Return the vertex if it exists"""
+        # return the vertex if it is in the graph
+        if key in self.vert_dict.keys():
+            return key
+        else:
+            raise ValueError("No vertex found!")
+
+    def add_edge(self, from_vert, to_vert, weight=0):
+        """Add an edge from vertex with key `key1` to vertex with key `key2` with a weight."""
+        if from_vert not in self.vert_dict or to_vert not in self.vert_dict:
+            # add it - or return an error (choice is up to you).
+            raise ValueError("One of the key doesn't exist!")
+        else:
+            # edge by making key2 a neighbor of key1
+            # and using the addNeighbor method of the Vertex class.
+            # Hint: the vertex f is stored in self.vert_dict[f].
+            self.vert_dict[from_vert].add_neighbor(self.vert_dict[to_vert], weight)
+            # self.vert_dict[to_vert].add_neighbor(self.vert_dict[from_vert], weight)
+
+    def get_vertices(self):
+        """Return all the vertices in the graph"""
+        return self.vert_dict.keys()
+
+    def get_all_edges(self):
+        """Return number of all edges in the graph"""
+        sum = 0
+        for v in self:
+            sum += v.get_edges()
+        return sum
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Challenge 1: Create the graph
+
+    g = Graph()
+
+    # Add your friends
+    g.addVertex("Friend 1")
+    g.addVertex("Friend 2")
+    g.addVertex("Friend 3")
+
+    # ...  add all 10 including you ...
+
+    # Add connections (non weighted edges for now)
+    g.addEdge("Friend 1", "Friend 2")
+    g.addEdge("Friend 2", "Friend 3")
+
+    # Challenge 1: Output the vertices & edges
+    # Print vertices
+    print("The vertices are: ", g.getVertices(), "\n")
+
+    print("The edges are: ")
+    for v in g:
+        for w in v.getNeighbors():
+            print("( %s , %s )" % (v.getId(), w.getId()))
