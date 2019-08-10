f42f1922acbdebb553c51d5123fe70d53cb78351
Nathan Pillai <natepill@gmail.com>
2019-08-10 14:48:42 -0700

    [added] recursive solution

--- Challenges/challenge_4/challenge_4.py
+++ Challenges/challenge_4/challenge_4.py
@@ -1,38 +1,43 @@
-def knapsack(C, items, n):
-  ''' A  method to determine the maximum value of the items included in the knapsack
-without exceeding the capacity  C
 
-    Parameters:
-    C= 50
-    items = (("boot", 10, 60),
-         ("tent", 20, 100),
-         ("water", 30, 120),
-         ("first aid", 15, 70))
-    Returns: max value
-'''
 
-def recursive_knapsack(items, capacity):
+def knapsack(capacity, items, n):
     '''
+    A  method to determine the maximum value of the items included in the knapsack
+  without exceeding the capacity  C
+
+      Parameters:
+      C= 50
+      items = (("boot", 10, 60),
+           ("tent", 20, 100),
+           ("water", 30, 120),
+           ("first aid", 15, 70))
+      Returns: max value
+
     inputs: Array of tuples, weights, item values and the maximum capacity of the bag
     output: the max value the bag may contain
     '''
-    if len(items) == 0 or capacity == 0:
+
+    if (n == 0 or capacity == 0):
         return 0
-    item = items[0]
-    if item[0] > capacity:
-        return recursive_knapsack(items[1:], capacity)
-    value_without = recursive_knapsack(items[1:], capacity)
-    value_with = recursive_knapsack(
-        items[1:], capacity - item[0]) + item[1]
-    return max(value_with, value_without)
+
+    if (items[n-1][1] > capacity):
+        # print(items[n-1][1])
+        return knapsack(capacity, items, n-1)
+
+    else:
+        return max(items[n-1][2] + knapsack(capacity - items[n-1][1], items, n-1 ),
+                    knapsack(capacity, items, n-1))
+
 
 
 if __name__ == "__main__":
 
 
-items = (("boot", 10, 60),
-         ("tent", 20, 100),
-         ("water", 30, 120),
-         ("first aid", 15, 70))
-# Capacity of knapsack: 50
-# The value of the optimal solution to the knapsack problem is V=230
+    items = (("boot", 10, 60),
+        ("tent", 20, 100),
+        ("water", 30, 120),
+        ("first aid", 15, 70))
+
+    print(knapsack(50, items, len(items)))
+    # Capacity of knapsack: 50
+    # The value of the optimal solution to the knapsack problem is V=230
